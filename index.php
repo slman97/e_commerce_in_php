774db66d7e0c2ac29b93517a2b32a55215d2ee3d
slman97 <slmanismael20@gmail.com>
2021-12-02 14:07:26 +0200

    TopSale

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include('file/header.php');
+include('file/_banner-area.php');
+include('file/_banner_adds.php');
+include('file/_special-price.php');
+include('file/_top-sale.php');
+include('file/_new-phones.php');
+include('file/_blogs.php');
+include('file/footer.php');
